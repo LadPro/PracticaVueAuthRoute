b696bcb8afa504cbfc301d0b5bddedffba836817
Joel Mora <joeljmorab@gmail.com>
2024-02-26 18:44:57 -0300

    agregado login 100% original hecho por mi para nada robado de una pagina de componentes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: LoginView
   },
   {
     path: '/about',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <h1 class>Salvenme de mi mismo</h1>
   </div>
 </template>
